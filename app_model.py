0a96679a4a622638ac104127929ee29c131ae57c
Lorenhdc <95498391+Lorenhdc@users.noreply.github.com>
2022-05-04 13:50:32 +0200

    seguimos cambiando

--- app_model.py
+++ app_model.py
@@ -5,9 +5,6 @@ from sklearn.linear_model import Lasso
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-
-os.chdir(os.path.dirname(__file__))
-
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
